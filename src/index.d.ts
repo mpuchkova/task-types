bb0bddf28aac68b6a1ce5793b0bc8cf119c5e379
mpuchkova <puchkovamm2001@mail.ru>
2022-04-09 15:14:57 +0300

    task-types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,21 +1,10 @@
-/*
-Типизируйте функцию getPersons, возвращающую массив объектов со следующими свойствами:
-- name - строка
-- age - число
-- gender - 'male' или 'female'
- */
-export function getPersons();
+type TPerson = {
+  name: string;
+  age: number;
+  gender: 'male' | 'female';
+};
+export function getPersons(): TPerson[];
 
-/*
-Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
-   1. Пользователь:
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   2. Сотрудник
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   - company- строка
- */
-export function personToString(person);
+type TEmployee = TPerson & { company: string };
+
+export function personToString(person: TPerson | TEmployee): string;
